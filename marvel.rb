14c348d1ca9dc7b22df2cb5e616d574354213533
garedean <garrettdolson@gmail.com>
2016-04-04 20:08:49 -0700

    Refactor Marvel class

--- marvel.rb
+++ marvel.rb
@@ -11,25 +11,27 @@
 # - Return updated code and steps to generate answers in email attachment.
 
 require 'digest/md5'
-require "net-http"
+require "net/http"
 require "uri"
+require 'byebug'
+require 'dotenv'
+Dotenv.load
 
 class Marvel
   attr_reader :public_key, :private_key
 
-  def initialize(options={})
-    @public_key='123'
-    @private_key='ABCD123'
+  def initialize(private_key: ENV['PRIVATE_KEY'])
+    @public_key  = 'acb9bf7870195e71cd36cfaea2a9099e'
+    @private_key = private_key
   end
 
   def get_the_characters
-    uri = URI.parse("http://gateway.marvel.com")
+    uri = URI.parse(
+      "http://gateway.marvel.com/v1/public/characters#{auth_params}")
 
-    http =  Net::HTTP.new(uri.host, uri.port)
-    request=Net::HTTP::Get.new("/v1/public/characters#{auth_params(public_key,private_key)}&limit=1")
-    @response = http.request(request)
+    @response = Net::HTTP.get_response(uri)
     @response.body
-rescue
+  rescue
     nil
   end
 
@@ -38,31 +40,36 @@ def people_in_comics(comic_ids_array = [])
     uri = URI.parse("http://gateway.marvel.com")
     http = Net::HTTP.new(uri.host, uri.port)
 
-
-    request = Net::HTTP::Get.new("/v1/public/characters#{auth_params}&limit=1&comics=#{comic_ids}")
+    request = Net::HTTP::Get.new(
+      "/v1/public/characters#{auth_params}&limit=1&comics=#{comic_ids}")
 
     @response = http.request(request)
-
     @response.body
   end
 
-  def auth_params(publick,privatek)
-      ts = Time.now.to_i
-      hash = Digest::MD5.hexdigest("#{ts}#{privatek}#{publick}")
-      "?ts=#{ts}#{ts}&apikey=#{publick}&hash=#{hash}"
+  private
+
+  def auth_params
+    ts   = Time.now.to_i
+    hash = Digest::MD5.hexdigest("#{ts}#{@private_key}#{@public_key}")
+    "?ts=#{ts}&apikey=#{@public_key}&hash=#{hash}"
   end
 
   def to_csv(array)
-  output = ''
-  is_first = true
-  array.each do |x|
-  unless is_first
-  output = output + ','
-  end
-  output = output + x.to_s
-  is_first = false
-  end
-  return output
-  end
+    output = ''
+    is_first = true
 
+    array.each do |x|
+      unless is_first
+        output = output + ','
+      end
+      output = output + x.to_s
+      is_first = false
+      end
+    return output
+  end
 end
+
+# puts Marvel.new.get_the_characters
+#comic_ids = [31068, 20367]
+# puts people_in_comics(comic_ids)
